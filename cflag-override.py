b733d66bc28a328c8162c855c6a8e0d30832b9e4
xCuri0 <zkqri0@gmail.com>
2019-09-16 15:09:11 +0530

    change cflag-override

--- cflag-override.py
+++ cflag-override.py
@@ -4,20 +4,20 @@ import sys
 import os
 import glob
 
-CC = sys.argv[1]
-args = sys.argv[2:]
+CC = sys.argv[2]
+args = sys.argv[3:]
 lines = []
 
 try:
     if 'CFFILE' in os.environ:
         lines = os.environ['CFFILE'].splitlines()
     else:
-        lines = open(os.environ['CFOVERRIDE']).readlines()
+        lines = open(sys.argv[1]).readlines()
     for l in lines:
         for a in args:
             if os.path.abspath(a) in [os.path.abspath(x) for x in glob.glob(l.split(':')[0])]:
                 args.extend(l.split(":")[1].split())
 except (IOError, IndexError):
-    print("Couldn't read CFOVERRIDE file")
+    print("Couldn't read CFLAG override file")
 
 sys.exit(subprocess.call([CC] + args))
--- cxxflag-override.py
+++ cxxflag-override.py
@@ -4,20 +4,20 @@ import sys
 import os
 import glob
 
-CXX = sys.argv[1]
-args = sys.argv[2:]
+CXX = sys.argv[2]
+args = sys.argv[3:]
 lines = []
 
 try:
     if 'CFFILE' in os.environ:
         lines = os.environ['CFFILE'].splitlines()
     else:
-        lines = open(os.environ['CFOVERRIDE']).readlines()
+        lines = open(sys.argv[1]).readlines()
     for l in lines:
         for a in args:
             if os.path.abspath(a) in [os.path.abspath(x) for x in glob.glob(l.split(':')[0])]:
                 args.extend(l.split(":")[1].split())
 except (IOError, IndexError):
-    print("Couldn't read CFOVERRIDE file")
+    print("Couldn't read CFLAG override file")
 
 sys.exit(subprocess.call([CXX] + args))
